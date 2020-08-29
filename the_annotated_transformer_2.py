e364910b14f3df62d037d5f9a7ff6eba7e95ca2f
basilwang <wanghuajie@yunwii.com>
2020-08-29 15:44:51 +0800

    use simple loss compute

--- the_annotated_transformer_2.py
+++ the_annotated_transformer_2.py
@@ -587,7 +587,7 @@ class MultiGPULossCompute:
         out_scatter = nn.parallel.scatter(out,
                                           target_gpus=self.devices)
         print("out_scatter:", out_scatter)
-        print("out_scatter's size:", len(out_scatter))
+        print("out_scatter's size:", torch.Tensor(out_scatter).size())
         out_grad = [[] for _ in out_scatter]
         targets = nn.parallel.scatter(targets,
                                       target_gpus=self.devices)
