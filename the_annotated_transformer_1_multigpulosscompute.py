00827e1c6455afa6c137383f4401f741c551a974
basilwang <wanghuajie@yunwii.com>
2020-08-29 17:48:16 +0800

    add new file to test multigpulosscompute

--- the_annotated_transformer_1_multigpulosscompute.py
+++ the_annotated_transformer_1_multigpulosscompute.py
@@ -650,12 +650,10 @@ devices = [0, 1]
 
 # Train the simple copy task.
 V = 11
-padding_idx = torch.IntTensor(0)
-padding_idx = padding_idx.cuda()
-criterion = LabelSmoothing(size=V, padding_idx=padding_idx, smoothing=0.0)
-criterion.cuda()
+criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
+criterion = criterion.cuda()
 model = make_model(V, V, N=2)
-model.cuda()
+model = model.cuda()
 model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
         torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
 
