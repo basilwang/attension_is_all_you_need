187efdee1d2376b061e81869e5005d4be453c380
basilwang <wanghuajie@yunwii.com>
2020-08-30 17:57:17 +0800

    seems find the problem

--- the_annotated_transformer_1_datapararell.py
+++ the_annotated_transformer_1_datapararell.py
@@ -458,7 +458,7 @@ def data_gen(V, batch, nbatches):
     for i in range(nbatches):
         data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
         data[:, 0] = 1
-        #data = data.cuda()
+        data = data.cuda()
         src = Variable(data, requires_grad=False)
         tgt = Variable(data, requires_grad=False)
         yield Batch(src, tgt, 0)
@@ -686,9 +686,9 @@ devices = [0, 1]
 # Train the simple copy task.
 V = 11
 criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
-#criterion.cuda()
+criterion.cuda()
 model = make_model(V, V, N=2)
-#model.cuda()
+model.cuda()
 model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
         torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
 # model_par = nn.DataParallel(model, device_ids=devices)
